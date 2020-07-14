72bd547a2506c4155ad8d602f473a6ef84f2436a
Hamzakh3 <hamkh3@gmail.com>
2020-07-14 16:13:35 +0500

    Add All files

---
+++ JavaScript/app.js
@@ -0,0 +1,16 @@
+// ------------------------ Chapter 1 ---------------------------------
+function greeting(){
+    alert("Hello Thanks For Visit our Website")
+}
+
+function jsAlert(){
+    alert("Error! Please enter a valid password")
+}
+function jsWelcomeAlert(){
+    alert("Welcome to JS land... \nHappy Coding!")
+}
+function sequenceAlert(){
+    alert("welcom to JS land.")
+    alert("Happy Codding!")
+}
+alert("Hello! I can run JS through my web browser's console")
